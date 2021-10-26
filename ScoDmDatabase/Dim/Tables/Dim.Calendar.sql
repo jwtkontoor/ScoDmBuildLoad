d947d94a99add0b800e3364e09c1d2a6bc3dc690
Jeff Tolle <jeffe.tolle-c@kontoorbrands.com>
2021-10-26 10:42:32 -0400

    Fix calendar

--- ScoDmDatabase/Dim/Tables/Dim.Calendar.sql
+++ ScoDmDatabase/Dim/Tables/Dim.Calendar.sql
@@ -4,22 +4,22 @@
     , [CalendarDateTimeDTS]			        DATETIME		        NOT NULL 	
     , [CalendarDTS]					        DATE		            NOT NULL 	
     , [CalendarDateDESC]				    VARCHAR(255)	        NOT NULL 
-	, [CalendarDayOfWeekID]  		        INT				        NOT NULL		    DEFAULT -1 
+	, [CalendarDayOfWeekNUM]  		        INT				        NOT NULL		    DEFAULT -1 
     , [CalendarDayOfWeekNM]			        VARCHAR(255)	        NOT NULL		    DEFAULT ('MISSING') 
-    , [CalendarDayOfMonthID]		        VARCHAR(255)	        NOT NULL		    DEFAULT ('MISSING') 
-    , [CalendarDayOfYearID]			        VARCHAR(255)	        NOT NULL		    DEFAULT ('MISSING') 
-    , [CalendarWeekID]				        INT				        NOT NULL		    DEFAULT -1 
+    , [CalendarDayOfMonthNUM]		        VARCHAR(255)	        NOT NULL		    DEFAULT ('MISSING') 
+    , [CalendarDayOfYearNUM]			    VARCHAR(255)	        NOT NULL		    DEFAULT ('MISSING') 
+    , [CalendarWeekNUM]				        INT				        NOT NULL		    DEFAULT -1 
     , [CalendarWeekNM]				        VARCHAR(255)	        NOT NULL		    DEFAULT ('MISSING') 
-    , [CalendarMonthID]				        INT				        NOT NULL		    DEFAULT -1 
+    , [CalendarMonthNUM]				    INT				        NOT NULL		    DEFAULT -1 
     , [CalendarMonthNM]				        VARCHAR(255)	        NOT NULL		    DEFAULT ('MISSING') 
-    , [CalendarQuarterID]			        INT				        NOT NULL		    DEFAULT -1 
+    , [CalendarQuarterNUM]			        INT				        NOT NULL		    DEFAULT -1 
     , [CalendarQuarterNM]			        VARCHAR(255)	        NOT NULL		    DEFAULT ('MISSING') 
-    , [CalendarQuarterFirstDayID]	        DATE			        NOT NULL		    
-    , [CalendarQuarterLastDayID]	        DATE			        NOT NULL		    
-    , [CalendarYearID]				        INT				        NOT NULL		    DEFAULT -1 
+    , [CalendarQuarterFirstDayDTS]	        DATE			        NOT NULL		    
+    , [CalendarQuarterLastDayDTS]	        DATE			        NOT NULL		    
+    , [CalendarYearNUM]				        INT				        NOT NULL		    DEFAULT -1 
     , [CalendarYearNM]				        VARCHAR(255)	        NOT NULL		    DEFAULT ('MISSING') 
-    , [CalendarYearFirstDayID]		        DATE			        NOT NULL		    DEFAULT '01-01-2021'
-    , [CalendarYearLastDayID]		        DATE			        NOT NULL		    DEFAULT '12-31-2021'
+    , [CalendarYearFirstDayDTS]		        DATE			        NOT NULL		    DEFAULT '01-01-2021'
+    , [CalendarYearLastDayDTS]		        DATE			        NOT NULL		    DEFAULT '12-31-2021'
     , [CalendarYearMonthNM]			        VARCHAR(255)	        NOT NULL		    DEFAULT ('MISSING') 
     , [CurrentDateFLG]				        VARCHAR(255)	        NOT NULL		    DEFAULT ('N')  
     , [DefinedHolidayFLG]			        VARCHAR(255)	        NOT NULL		    DEFAULT ('N') 
@@ -34,4 +34,4 @@ GO
 
 
 CREATE UNIQUE NONCLUSTERED INDEX [UIX_Dim_Calendar_CalendarDT]
-    ON [Dim].[Calendar]([CalendarDT] ASC);
+    ON [Dim].[Calendar]([CalendarDTS] ASC);
